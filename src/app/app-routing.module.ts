3a6b61b5c87314c573391b1478c951ffa46c8c77
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-23 16:20:11 +0200

    added routing to components CallbackComponent ForbiddenComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { authGuard } from './core/guard/auth.guard';
 import { adminGuard } from './core/guard/admin.guard';
+import {CallbackComponent} from './callback/callback.component';
+import {ForbiddenComponent} from './forbidden/forbidden.component';
 
 const routes: Routes = [
   {
@@ -27,6 +29,14 @@ const routes: Routes = [
     path: 'appointment',
     loadChildren:() => import('./appointment/appointment.module').then(module => module.AppointmentModule),
     canActivate:[authGuard]
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent
+  },
+  {
+    path: 'forbidden',
+    component: ForbiddenComponent
   }
 ];
 
