e252d148cb1377bad9b6c50be1d98cf6a8e10166
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-23 16:18:26 +0200

    Added a callback functionality

---
+++ src/app/callback/callback.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-callback',
+  standalone: false,
+  templateUrl: './callback.component.html',
+  styleUrl: './callback.component.scss'
+})
+export class CallbackComponent implements OnInit {
+  constructor(private _router: Router) {
+  }
+  ngOnInit(): void {
+    //this._router.navigate(['/']);
+  }
+}
